c04552ea157f1ae9183cfdf29ac32c22da891746
Edi Prasetyi <ediprasetiyo2@gmail.com>
2022-11-26 10:29:14 +0700

    Update payment

--- application/views/front/donasi/detail_donasi.php
+++ application/views/front/donasi/detail_donasi.php
@@ -16,7 +16,7 @@ $page      = $this->page_model->get_page();
     <title><?php echo $meta->title ?> | <?php echo $meta->tagline ?></title>
     <link rel="shortcut icon" href="<?php echo base_url('assets/img/logo/' . $meta->favicon); ?>">
     <meta name="description" content="<?php echo $deskripsi ?>">
-    <meta name="keywords" content="<?php echo $meta->title . ',' . $donasi->keywords ?>">
+    <meta name="keywords" content="<?php echo $meta->title . ',' . $donasi->donasi_keywords ?>">
     <meta name="author" content="<?php echo $meta->title ?>">
     <meta name="google-site-verification" content="<?php echo $meta->google_meta ?>" />
     <meta name="msvalidate.01" content="<?php echo $meta->bing_meta ?>" />
